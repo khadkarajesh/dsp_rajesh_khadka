3996eddca0005932608b0ff09e3cfe84a4cb8a36
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2021-11-10 10:56:12 +0100

    refactor preprocessing process

--- app/models/preprocess.py
+++ app/models/preprocess.py
@@ -1,3 +1,6 @@
+import os
+
+import numpy as np
 from pathlib import Path
 from typing import List
 
@@ -8,13 +11,58 @@ from sklearn.compose import make_column_selector as selector
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
+ENCODER = "encoder.joblib"
 
 
-def get_encoder(path_to_save_encoder) -> OneHotEncoder:
-    encoder_path = path_to_save_encoder / 'encoder.joblib'
+def handle_categorical_data(dataframe: pd.DataFrame, model_dir: Path):
+    columns = get_categorical_columns(dataframe)
+    categorical_dataframe = dataframe[columns].copy()
+    filled_unknowns_dataframe = fill_unknown(columns, categorical_dataframe)
+    encoder = get_encoder(model_dir=model_dir)
+    encoder.fit(filled_unknowns_dataframe)
+    encoded_sparse_matrix = encoder.transform(filled_unknowns_dataframe)
+    encoded_data_columns = encoder.get_feature_names(columns)
+    encoded_categorical_data_df = pd.DataFrame.sparse.from_spmatrix(data=encoded_sparse_matrix,
+                                                                    columns=encoded_data_columns,
+                                                                    index=filled_unknowns_dataframe.index)
+    return encoded_categorical_data_df
+
+
+def get_categorical_columns(dataframe: pd.DataFrame):
+    return dataframe.select_dtypes(exclude=[np.number]).columns.tolist()
+
+
+def get_continuous_columns(dataframe: pd.DataFrame):
+    return dataframe.select_dtypes(exclude=[np.object]).columns.tolist()
+
+
+def handle_numerical_data(dataframe: pd.DataFrame):
+    columns = get_continuous_columns(dataframe)
+    imputed_dataframe = dataframe[columns].copy()
+
+    simple_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imputed_dataframe[columns] = simple_imputer.fit_transform(imputed_dataframe[columns])
+
+    scalar = StandardScaler()
+    imputed_dataframe[columns] = scalar.fit_transform(imputed_dataframe[columns])
+
+    return imputed_dataframe
+
+
+def preprocess(X: pd.DataFrame, model_dir: Path) -> pd.DataFrame:
+    numerical_dataframe = handle_numerical_data(dataframe=X)
+    categorical_dataframe = handle_categorical_data(dataframe=X, model_dir=model_dir)
+    merged_dataframe = numerical_dataframe.join(categorical_dataframe)
+    return merged_dataframe
+
+
+def get_encoder(model_dir: Path) -> OneHotEncoder:
+    encoder_path = Path(model_dir / ENCODER)
     if encoder_path.exists(): return joblib.load(encoder_path)
-    encoder = OneHotEncoder(handle_unknown="ignore")
-    save_encoder(path_to_save_encoder, encoder)
+    encoder = OneHotEncoder(handle_unknown='ignore', dtype=int, sparse=True)
+    joblib.dump(encoder, model_dir / ENCODER)
     return encoder
 
 
@@ -32,7 +80,7 @@ def get_column_transformer(path_to_save_encoder) -> ColumnTransformer:
 
 def fill_unknown(columns: List, dataframe) -> pd.DataFrame:
     for column in columns:
-        dataframe[column].fillna('Unknown')
+        dataframe[column] = dataframe[column].fillna('Unknown')
     return dataframe
 
 
@@ -51,7 +99,3 @@ def impute_numerical_value(columns: List, dataframe, **kwargs) -> pd.DataFrame:
             value = dataframe[column].median()
         dataframe[column].fillna(value)
     return dataframe
-
-
-def save_encoder(path_to_save_encoder, encoder):
-    joblib.dump(encoder, path_to_save_encoder / 'encoder.joblib')
--- run.py
+++ run.py
@@ -5,14 +5,16 @@ import pandas as pd
 from app.models.inference import predict
 from dotenv import load_dotenv
 
-from app.models.preprocess import get_column_transformer
+from app.models.preprocess import get_column_transformer, preprocess
 from app.models.train import train_model
 
 load_dotenv('.env')
 
-model_path = Path(os.environ.get('MODEL_PATH'))
-# train model
-train_model(Path(os.environ.get('DATA_PATH'), 'train.csv'), get_column_transformer(model_path), model_path)
+# model_path = Path(os.environ.get('MODEL_PATH'))
+# # train model
+# train_model(Path(os.environ.get('DATA_PATH'), 'train.csv'), get_column_transformer(model_path), model_path)
+#
+# # predict model
+# predict(pd.read_csv(Path(os.environ.get('DATA_PATH'), 'test.csv')), model_path / 'model.joblib')
 
-# predict model
-predict(pd.read_csv(Path(os.environ.get('DATA_PATH'), 'test.csv')), model_path / 'model.joblib')
+preprocess(pd.read_csv(Path(os.environ.get('DATA_PATH'), 'train.csv')), Path("app/models"))
